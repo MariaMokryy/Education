b9664e03ed4e29c9f4567a7c47567ce170f555de
Мария Мокрый <masha.mokryy@mail.ru>
2023-01-23 15:13:56 +0800

    reorganize structure

--- main/models.py
+++ main/models.py
@@ -8,13 +8,25 @@ class Employee(models.Model):
     lastname = models.TextField
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name = 'сотрудник'
+        verbose_name_plural = 'сотрудники'
+
 
 class Module(models.Model):
     name = models.TextField(unique=True)
+
+    class Meta:
+        verbose_name = 'модуль'
+        verbose_name_plural = 'модули'
 
 
 class Course(models.Model):
     name = models.TextField(unique=True)
     code = models.TextField(unique=True)
     module = models.ForeignKey(Module, null=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
 
